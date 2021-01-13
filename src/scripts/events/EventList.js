66a51ba808db29f78c1798448c013f365b2712e2
Aaron Resch <aaronmresch@gmail.com>
2021-01-13 15:00:48 -0600

    added event listener for eventStateChanged

--- src/scripts/events/EventList.js
+++ src/scripts/events/EventList.js
@@ -3,29 +3,26 @@ import { useEvents } from './eventProvider.js';
 import { useFriends } from '../friends/friendDataProvider.js';
 
 const eventHub = document.querySelector('.container');
-const testButton = document.getElementById('test')
-
-eventHub.addEventListener('')
-
 
 export const EventList = () => {
+  console.log('BANG')
   const activeUserId = parseInt(sessionStorage.getItem('activeUser'));
   const events = useEvents();
   const friends = useFriends();
-  
+
   // array of user's events
   const userEvents = events.filter((e) => activeUserId === e.userId);
-  
+
   if (events.length > 0) {
     // add a class designation to each user event object
     userEvents.forEach((e) => {
       e.class = 'userEvent';
     });
-    
+
     const userFriends = friends.filter((f) => activeUserId === f.userId);
-    
+
     const friendIds = userFriends.map((f) => f.friendId);
-    
+
     // array of events for all friends
     let fEvents = [];
     friendIds.forEach((i) => {
@@ -36,15 +33,15 @@ export const EventList = () => {
         fEvents.push(event);
       }
     });
-    
+
     if (fEvents.length > 0) {
       // add a class designation to each friend event object
       fEvents.forEach((e) => {
         e.class = 'friendEvent';
       });
-      
+
       const allEvents = userEvents.concat(fEvents);
-      
+
       return `
       <div class="event-list__top-row">
       <button id="newEvent">New Event</button>      
@@ -77,7 +74,9 @@ export const EventList = () => {
 
 const render = (events) => {
   return events
-  .sort((a, b) => a.date.localeCompare(b.date))
-  .map((e) => Event(e))
-  .join('');
+    .sort((a, b) => a.date.localeCompare(b.date))
+    .map((e) => Event(e))
+    .join('');
 };
+
+eventHub.addEventListener('eventStateChanged', EventList);
