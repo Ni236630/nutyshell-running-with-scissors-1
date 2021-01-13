0e5d8398eb3df817e6e9e0aa19eb61295582a64c
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-01-13 09:45:45 -0600

    preparing for merge

---
+++ src/scripts/articles/articleDataProvider.js
@@ -0,0 +1,45 @@
+// Ryan DeVault - Purpose: Retrieve, save and/or delete articles information from the database.json file within the api folder
+
+// Local Variables
+let articles = [];
+
+// Selectors
+const eventHub = document.querySelector('container');
+
+// Event Dispatcher
+const dispatchStateChangeEvent = () => {
+    const articleStateChangedEvent = new CustomEvent("articleStateChanged");
+
+    eventHub.dispatchEvent();
+};
+
+// GETs articles from database.json and stores them in the local articles variable
+export const getArticles = () => {
+    return fetch("http://localhost:8088/articles")
+        .then(response => response.json())
+        .then(parsedArticles => articles = parsedArticles);
+};
+
+// Returns a copy of the array in the articles variable
+export const useArticles = () => articles.slice();
+
+// Saves new articles to the API
+export const saveArticle = article => {
+    return fetch('http://localhost:8088/articles', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(article)
+    })
+    .then(getArticles)
+    .then(dispatchStateChangeEvent);
+};
+
+// Deletes articles from the API
+export const deleteArticle = articleId => {
+    return fetch(`http://localhost:8088/articles/${articleId}`,{
+        method: "DELETE"
+    })
+    .then(getArticles);
+};
--- src/scripts/users/userDataProvider.js
+++ src/scripts/users/userDataProvider.js
@@ -1,36 +1,14 @@
-// Ryan DeVault - Fetches users information from the database.json file within the api folder
+// Ryan DeVault - Purpose: Retrieve users information from the database.json file within the api folder
 
 // Local Variables
 let users = [];
 
-// Selectors
-const eventHub = document.querySelector('.container');
-
-// Returns a copy of the users array
-export const useUsers = () => users.slice();
-
+// GETs users from database.json and stores them in the local users variable
 export const getUsers = () => {
     return fetch("http://localhost:8088/users")
         .then(response => response.json())
         .then(parsedUsers => users = parsedUsers);
 };
 
-const dispatchStateChangeEvent = () => {
-    const userStateChangeEvent = new CustomEvent("userStateChanged");
-
-    eventHub.dispatchEvent(userStateChangeEvent);
-};
-    
-// WAIT IS THIS NECESARY? Auth folder seems to already handle saving new users
-// Saves new users to the API
-export const saveUser = user => {
-    return fetch('http://localhost:8088/users', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
-    })
-    .then(getUsers)
-    .then(dispatchStateChangeEvent);
-};
+// Returns a copy of the array in the users variable
+export const useUsers = () => users.slice();
