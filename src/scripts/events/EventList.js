6d67af0eafd25613750697c34f72f5d849c18d01
Aaron Resch <aaronmresch@gmail.com>
2021-01-14 12:44:32 -0600

    refactored eventList to display ALL friends events (was just showing one)

--- src/scripts/events/EventList.js
+++ src/scripts/events/EventList.js
@@ -29,11 +29,13 @@ export const EventList = () => {
     // array of events for all friends
     let fEvents = [];
     friendIds.forEach((i) => {
-      let event = events.find((e) => i === e.userId);
-      if (!event) {
+      let eventList = events.filter((e) => i === e.userId);
+      if (eventList.length === 0) {
         return;
       } else {
-        fEvents.push(event);
+        eventList.forEach((e) => {
+          fEvents.push(e);
+        });
       }
     });
 
