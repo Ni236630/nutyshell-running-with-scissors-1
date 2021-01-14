a7843a856a897da0de325f04208461ebd0d0868a
Ryan Youngblood <rryb77@gmail.com>
2021-01-14 16:04:45 -0600

    Started logic for private messages, can isolate username from the @name in the message

--- src/scripts/messages/messageList.js
+++ src/scripts/messages/messageList.js
@@ -4,6 +4,7 @@
 import { deleteMessage, getMessages, saveMessage, useMessages } from "./messagesDataProvider.js"
 import { deleteUserMessage, saveUserMessage, useUserMessages } from "./userMessagesDataProvider.js"
 import { useUsers } from '../users/userDataProvider.js'
+import { useFriends } from "../friends/friendDataProvider.js"
 
 // Setup the arrays to plug data in
 let allUsers = []
@@ -62,9 +63,20 @@ export const messageList = () => {
     `
 }
 
-//-----------------------------------------------//
-//               Event Listeners                 //
-//-----------------------------------------------//
+// eventHub.addEventListener('keyup', e => {
+//     if (e.target.id === 'chatTextInput' && e.target.value.startsWith("@")) {
+//         console.log('its going!')
+//         const chatBar = document.querySelector('#chatTextInput')
+//         const users = useUsers()
+//         const friends = useFriends()
+//         const activeUser = parseInt(sessionStorage.getItem('activeUser'))
+
+//         let friendResults = []
+//         let chatInput = chatBar.value.toLowerCase()
+
+
+//     }
+// })
 
 eventHub.addEventListener("click", clickEvent => {
     
@@ -80,58 +92,81 @@ eventHub.addEventListener("click", clickEvent => {
         if (buttonName === "Entry") {
             // Grab the user text
             const userText = document.querySelector("#chatTextInput")
+
             // Get the active user ID
             activeUser = parseInt(sessionStorage.getItem('activeUser'));
 
-            // Create a new object to pass to the messages table in the json DB
-            const newMessage = {
+            // If the message is meant to be private then...
+            if (userText.value.startsWith("@")) {
+                
+                // Get friends
+                const friends = useFriends()
+
+                // Split the input to grab the username
+                const friendTag = userText.value.split(" ")
+
+                // Remove the @ so the name can be used to search
+                const friendName = friendTag[0].substring(1)
+                console.log(`My friend is: `, friendName)
+
+                // Need to get back all friends table entries with all .userId === activeUser (activeUser already stores current user ID) -----map friends?
+            
+            // If there was no '@' then the message is public so do this...
+            } else {
+               
+                // Create a new object to pass to the messages table in the json DB
+                const newMessage = {
                 message: userText.value,
                 timestamp: Date.now()
-            }
+                }
 
-            // Clear out the input box
-            userText.value = ""
-            
-            // Send the new message to the json DB
-            saveMessage(newMessage)
-                // Update messages so the new ID can be accessed
-                .then(getMessages)
-                .then(() => {
-                    // Grab the last entry from the array
-                    const messages = useMessages().pop()
-                    
-                    // Create a new object to pass the new join table to the json DB
-                    const newUserMessage = {
-                        userId: activeUser,
-                        messageId: messages.id,
-                        recipientId: 0
-                    }
+                // Clear out the input box
+                userText.value = ""
+                
+                // Send the new message to the json DB
+                saveMessage(newMessage)
+                    // Update messages so the new ID can be accessed
+                    .then(getMessages)
+                    .then(() => {
+                        // Grab the last entry from the array
+                        const messages = useMessages().pop()
+                        
+                        // Create a new object to pass the new join table to the json DB
+                        const newUserMessage = {
+                            userId: activeUser,
+                            messageId: messages.id,
+                            recipientId: 0
+                        }
 
-                    // Send the new object to the userMessages table in the json DB
-                    saveUserMessage(newUserMessage)
-                })
+                        // Send the new object to the userMessages table in the json DB
+                        saveUserMessage(newUserMessage)
+                    })
+                }
 
         // If that button is the Delete button then...
         } else if (buttonName === "Delete") {
             
             deleteUserMessage(userMessageId)
-            deleteMessage(messageId)
+            .then(() => {
+                deleteMessage(messageId)
+            })
+            
         }
     }
 })
 
 // Event listener for public/private filtering. Needs work!
-eventHub.addEventListener("change", e => {
+// eventHub.addEventListener("change", e => {
 
-    if (e.target.id === "privacySelect") {
-        let privacyOptionChosen = e.target.value
+//     if (e.target.id === "privacySelect") {
+//         let privacyOptionChosen = e.target.value
 
-        if (privacyOptionChosen = 1){
-            console.log(`Private was chosen`)
-            chatText.innerHTML = "Private"
-        } else {
-            console.log(`Public was chosen`)
-            chatText.innerHTML = 'Public'
-        }
-    }
-})
+//         if (privacyOptionChosen = 1){
+//             console.log(`Private was chosen`)
+//             chatText.innerHTML = "Private"
+//         } else {
+//             console.log(`Public was chosen`)
+//             chatText.innerHTML = 'Public'
+//         }
+//     }
+// })
