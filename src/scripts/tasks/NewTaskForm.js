ab3cccd23bcef8e4877608336df343ff83a9583b
Ni236630 <nicholas.douso@gmail.com>
2021-01-14 11:58:56 -0600

    floundered on saveEvent; fixed

--- src/scripts/tasks/NewTaskForm.js
+++ src/scripts/tasks/NewTaskForm.js
@@ -3,7 +3,7 @@
 
 /*    Imports     */
 
-//import { saveTask } from "./taskDataProvider.js";
+import { saveTask } from "./taskDataProvider.js";
 
 
 /*    Listeners     */
@@ -12,14 +12,37 @@ const eventHub = document.querySelector(".container")
 //listens for save button to be clicked
 const dialogClose = document.querySelector(".container")
 
+//listener for saving tasks
+
+eventHub.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.id === "saveTask") {
+    const name = document.getElementById("taskName").value;
+    const completionDate = document.getElementById("taskDate").value;
+    const userId = parseInt(sessionStorage.getItem('activeUser'))
+    const isComplete = "false"
+
+    const newTask = {
+      userId:userId,
+      name:name,
+      completionDate:completionDate,
+      isComplete:isComplete
+    }
+    
+   return saveTask(newTask)
+
+  }
+});
 /*    events     */
+
+//closes dialog box
 dialogClose.addEventListener("click",event =>{
   if(event.target.id === "closeDialog__task"){
     const dialog = document.getElementById("taskDialog")
-    return dialog.closest()
+    return dialog.close()
   }
 })
 
+//opens dialog box
 eventHub.addEventListener("newTaskClicked", customEvent =>{
 
     const taskDialog = document.getElementById("taskDialog")
@@ -33,7 +56,7 @@ eventHub.addEventListener("newTaskClicked", customEvent =>{
           <label for="taskName">Task Name:<label>
           <input class="taskNameInput" type"text" name="taskName" id="taskName">
           <label for="completionDate">Due Date:<label>
-          <input class="taskDateInput" type"date" name="completionDate" id="taskDate">
+          <input class="taskDateInput" type="date" name="completionDate" id="taskDate">
           <button id="saveTask">Save</button>
         </form>
       </div>`
